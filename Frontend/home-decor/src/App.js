8645da8d005442f4e237ffe1b7e403b779d3c4a6
Shivam <shivampandey88@hotmail.com>
2023-06-05 22:24:41 +0530

    Individual product page desktop

--- Frontend/home-decor/src/App.js
+++ Frontend/home-decor/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Individual from './Components/Individual';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+        <Individual />
     </div>
   );
 }
