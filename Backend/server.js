217b526acb25b2f488ac11d2c3941ff4071539a7
Shivam <shivampandey88@hotmail.com>
2023-06-13 15:17:54 +0530

    Database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,16 @@
 const app = require("./app")
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 dotenv.config({path: "./Protect/config.env"})
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DB_PASSWORD)
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+}).then(function(connection){
+    console.log("Connected to database")
+})  
 
 const port = process.env.PORT || 8080
 app.listen(port,function(req,res){
