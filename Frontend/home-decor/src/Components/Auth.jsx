1ea7070543de59762859310143b83d2a5c089f19
Shivam <shivampandey88@hotmail.com>
2023-06-16 19:41:55 +0530

    Complete auth frontend UI

--- Frontend/home-decor/src/Components/Auth.jsx
+++ Frontend/home-decor/src/Components/Auth.jsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Auth(){
+    const [login, setLogin] = useState(false)
+
+    function showlogin(){
+        setLogin(true)
+    }
+
+    function showsignup(){
+        setLogin(false)
+    }
+
     return (
-        <>
-            <div className="auth-ui">
-                    <h1 style={{textAlign: "center", marginTop: "2vh", marginBottom: "2vh"}}>Signup/Login</h1>
-                    <label id="email" for="email" style={{marginTop: "1vh", marginBottom: "1vh"}}>Email:</label>
-                    <input type="text" name="email" placeholder="Enter your email" style={{width: "10vw", height: "3vh"}}/>
-                    <label id="pass" for="password" style={{marginTop: "1vh", marginBottom: "1vh"}}>Password:</label>
-                    <input type="password" name="password" placeholder="Enter your password" style={{width: "10vw", height: "3vh"}}/>
-                    <label for="Buyer" style={{marginTop: "1vh", marginBottom: "1vh"}}> I am a: Buyer</label>
-                    <input type="checkbox" name="Buyer" value="Buyer" />
-                    <label for="Seller" style={{marginTop: "1vh", marginBottom: "1vh"}}> I am a: Seller</label>
-                    <input type="checkbox" name="Seller" value="Seller" />
-                    <button className="Single-product-button">Submit</button>
-            </div>
+        <> 
+           <div className="auth-ui">
+                <div>
+                    <button className="form-action-button" onClick={showlogin}>Login</button>
+                    <button className="form-action-button" onClick={showsignup}>Sign up</button>
+                </div>
+                { !login ? (
+                <div className="form-design">
+                    <label className="form-label" for="signup-email">Enter email:</label>
+                    <input className="form-input" type="email"/>
+                    <label className="form-label" for="signup-pass">Enter password:</label>
+                    <input className="form-input" type="password"/>
+                    <label className="form-label" for="signup-passconf">Confirm password:</label>
+                    <input className="form-input" type="password"/>
+                    <label for="type" className="form-label">Choose your account type</label>
+                    <select name="type" className="form-dropdown">
+                        <option value="Select">Select</option>
+                        <option value="Buyer">Buyer</option>
+                        <option value="Seller">Seller</option>
+                    </select>
+                    <button className="form-button">Sign up</button>
+                </div>
+                ) : (
+                <div className="form-design"> 
+                    <lable className="form-label" for="login-email">Enter email:</lable>
+                    <input className="form-input" type="email"/>
+                    <label className="form-label" for="login-pass">Enter password:</label>
+                    <input className="form-input" type="password"/>
+                    <button className="form-button">Login</button>
+                </div>
+                )}
+           </div>
         </>
     )
 }
